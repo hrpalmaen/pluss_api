86afe1e2df701d16546e218551ce66a0e24083b0
hrpalmaen <hrpalmaen@gmail.com>
2020-02-10 23:00:19 -0500

    Modify: change view and serializer of quotation

--- app/serializers/quotation.py
+++ app/serializers/quotation.py
@@ -1,14 +1,29 @@
-from rest_framework.serializers import ModelSerializer
+# from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework import serializers
 from app.serializers import ClientSerializer, UserSerializer
 from app.models import Quotation, QuotationTemp
 
-class QuotationSerializer(ModelSerializer):
+class QuotationSerializer(serializers.ModelSerializer):
     '''
     Serializador para cotizaciones
     '''
+    client_name = serializers.CharField(source='client.name', read_only = True)
+    user_name = serializers.CharField(source='user.first_name', read_only = True)
     class Meta:
         model = Quotation
-        fields = ('__all__')
+        fields = (
+            'id',
+            'client',
+            'client_name',
+            'user',
+            'user_name',
+            'delivery_time',
+            'pay_format',
+            'units',
+            'status',
+            'products',
+            'date_created'
+        )
 
 class getQuotationSerializer(QuotationSerializer):
     '''
@@ -17,7 +32,7 @@ class getQuotationSerializer(QuotationSerializer):
     client = ClientSerializer()
     user = UserSerializer()
 
-class QuotationTempSerializer(ModelSerializer):
+class QuotationTempSerializer(serializers.ModelSerializer):
     '''
     Serializador para cotizaciones temporales
     '''
