470a4fe0eba945116c7610891a232ffe59998ebf
Hellen Palma <hrpalmaen@gmail.com>
2020-03-02 17:05:32 -0500

    Modify: Send mail

--- app/views/quotation.py
+++ app/views/quotation.py
@@ -5,16 +5,15 @@ from rest_framework import status
 from app.models import Quotation, Client
 from app.serializers import QuotationSerializer, QuotationFilter, getQuotationSerializer
 
-# from django.core.mail import send_mail
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.application import MIMEApplication
-
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+# import smtplib
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.application import MIMEApplication
 
 from django.conf import settings
 
 import pdfkit
-
 import re
 from django.http import HttpResponse
 
@@ -37,11 +36,10 @@ class QuotationView(ModelViewSet):
         data = request.data
         errors = {}
         obl_alert =  'Este campo es requerido'
-        file_attachment = self.pdf(request)
-        print('aaa: ', file_attachment)
+        print('obl_alert: ', obl_alert)
+        
 
         if not data['subject']:
-            print('entro al subject')
             errors['subject'] = obl_alert
         if not data['message']:
             errors['message'] = obl_alert
@@ -50,8 +48,15 @@ class QuotationView(ModelViewSet):
         if errors:
             return Response(errors, status=400)
 
-
+        attachments = []
+        try:
+            # pdfkit.from_url('https://www.pythoncircle.com/post/711/hello-word-in-django-2-how-to-start-with-django-2/', "prueba.pdf")
+            file_attachment = self.pdf(request)
+            print('file_attachment: ', file_attachment)
+        except Exception as e:
+            print('error pdf', e)
         if data['status'] == "Finalizado":
+
             try:
                 current_client = Client.objects.get(id=data['client'])
                 print('current_client', current_client)
@@ -60,26 +65,34 @@ class QuotationView(ModelViewSet):
 
                 if not validate_email:
                     return Response({'error': 'El email asociado al cliente no es válido'})
+                    
                 try:
-                    pdf_file = MIMEApplication(file_attachment, "pdf")
-                    pdf_file.add_header('content-Disposition', 'attachment', filename'cotizacion.pdf)
+                    content = open(file_attachment, 'rb').read() 
+                    attachment = (file_attachment, content, 'application/pdf')
+                    attachments.append(attachment)
+                    print('attachments: ', attachments)
+                except Exception as e:
+                    print('error pdf', e)
+
                 try:
                     print('email', email)
-
-                    msg = MIMEMultipart()
-                    msg['From'] = settings.EMAIL_HOST_USER
-                    msg['To'] = email
-                    msg['Subject'] = 'prueba'
-
-                    msg.attach(pdf_file)
 
                     # send_mail(
                     #     subject= 'prueba',
                     #     message='envio de la url de la cotización',
                     #     from_email= settings.EMAIL_HOST_USER,
-                    #     recipient_list=[email],
-                    #     attach('design.png', img_data, 'image/png')
+                    #     recipient_list=[email]
                     # )
+
+                    email = EmailMessage(
+                        'prueba con adjunto',
+                        'prueba no se que va acá',
+                        settings.EMAIL_HOST_USER,
+                        [email],
+                        attachments=attachments
+                    )                   
+                    email.send()
+
                 except Exception as e:
                     print('error en send_email', e)
             except Exception as e:
@@ -106,9 +119,10 @@ class QuotationView(ModelViewSet):
             'no-outline': None
         }
 
-        print('*******ENTRO*******')
+        print('entro al pdf')
         pdf = pdfkit.from_url('https://www.pythoncircle.com/post/470/generating-and-returning-pdf-as-response-in-django/', False)#, options=pdf_settings)
-        print('*******SALIO*******')
+        print('pdf: ', pdf)
         response = HttpResponse(pdf, content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename="Cotizacion.pdf'#.format('export')
         return response
+
